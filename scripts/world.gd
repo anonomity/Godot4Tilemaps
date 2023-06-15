9ff0e01eed62e945530cb1911f1af9f7f0362239
Jackie <jlauravelez@gmail.com>
2023-06-15 16:23:11 +0200

    ep5

--- scripts/world.gd
+++ scripts/world.gd
@@ -6,6 +6,13 @@ var ground_layer = 1
 var environment_layer = 2
 
 var can_place_seed_custom_data = "can_place_seeds"
+var can_place_dirt_custom_data = "can_place_dirt"
+
+enum FARMING_MODES {SEEDS, DIRT}
+
+var farming_mode_state = FARMING_MODES.DIRT
+
+var dirt_tiles = []
 
 func _ready():
 	pass
@@ -13,28 +20,39 @@ func _ready():
 
 
 func _input(event):
+	if Input.is_action_just_pressed("toggle_dirt"):
+		farming_mode_state = FARMING_MODES.DIRT
+		print("dirt")
+	if Input.is_action_just_pressed("toggle_seeds"):
+		farming_mode_state = FARMING_MODES.SEEDS
+		print("seeds")
 	if Input.is_action_just_pressed("click"):
 		
 		var mouse_pos : Vector2 = get_global_mouse_position()
-		
-#		print("global mouse position: ", mouse_pos)
-		
 		var tile_mouse_pos : Vector2i = tile_map.local_to_map(mouse_pos)
-		
-#		print("tile mouse position ", tile_mouse_pos)
-		
 		var source_id : int = 0
 		
-		var atlas_coord : Vector2i= Vector2i(11,1)
+		if farming_mode_state == FARMING_MODES.SEEDS:
+			var atlas_coord : Vector2i= Vector2i(11,1)
+			if retrieving_custom_data(tile_mouse_pos, can_place_seed_custom_data, ground_layer):
+				tile_map.set_cell(environment_layer, tile_mouse_pos, source_id, atlas_coord)
+		elif farming_mode_state == FARMING_MODES.DIRT:
+			if retrieving_custom_data(tile_mouse_pos,can_place_dirt_custom_data,ground_layer):
+				dirt_tiles.append(tile_mouse_pos)
+				tile_map.set_cells_terrain_connect(ground_layer, dirt_tiles, 2,0)
 		
-		var tile_data : TileData = tile_map.get_cell_tile_data(ground_layer, tile_mouse_pos)
-		
-		if tile_data:
-			var can_place_seeds = tile_data.get_custom_data(can_place_seed_custom_data)
-			if can_place_seeds:
-				tile_map.set_cell(environment_layer, tile_mouse_pos, source_id,  atlas_coord)
-			else:
-				print("can't place here!")
-		else:
-			print("no tile data")
 
+func retrieving_custom_data(tile_mouse_pos, custom_data_layer, layer):
+	var tile_data : TileData = tile_map.get_cell_tile_data(layer, tile_mouse_pos)
+	if tile_data:
+		return tile_data.get_custom_data(custom_data_layer)
+	else:
+		return false
+	
+	
+	
+	
+	
+	
+	
+	
