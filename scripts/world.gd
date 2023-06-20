c845cb0c8fb30fe73101c0008cbae3cb19921015
Jackie <jlauravelez@gmail.com>
2023-06-20 10:34:25 +0200

    plant growth stages

--- scripts/world.gd
+++ scripts/world.gd
@@ -14,12 +14,8 @@ var farming_mode_state = FARMING_MODES.DIRT
 
 var dirt_tiles = []
 
-func _ready():
-	pass
 
-
-
-func _input(event):
+func _input(_event):
 	if Input.is_action_just_pressed("toggle_dirt"):
 		farming_mode_state = FARMING_MODES.DIRT
 		print("dirt")
@@ -30,12 +26,13 @@ func _input(event):
 		
 		var mouse_pos : Vector2 = get_global_mouse_position()
 		var tile_mouse_pos : Vector2i = tile_map.local_to_map(mouse_pos)
-		var source_id : int = 0
 		
 		if farming_mode_state == FARMING_MODES.SEEDS:
 			var atlas_coord : Vector2i= Vector2i(11,1)
 			if retrieving_custom_data(tile_mouse_pos, can_place_seed_custom_data, ground_layer):
-				tile_map.set_cell(environment_layer, tile_mouse_pos, source_id, atlas_coord)
+				var level : int = 0
+				var final_seed_level : int = 3
+				handle_seed(tile_mouse_pos, level, atlas_coord, final_seed_level)
 		elif farming_mode_state == FARMING_MODES.DIRT:
 			if retrieving_custom_data(tile_mouse_pos,can_place_dirt_custom_data,ground_layer):
 				dirt_tiles.append(tile_mouse_pos)
@@ -52,7 +49,14 @@ func retrieving_custom_data(tile_mouse_pos, custom_data_layer, layer):
 	
 	
 	
+func handle_seed(tile_map_pos, level, atlas_coord, final_seed_level):
+	var source_id : int = 0
+	tile_map.set_cell(environment_layer, tile_map_pos, source_id, atlas_coord)
 	
+	await get_tree().create_timer(5.0).timeout
 	
-	
-	
+	if level == final_seed_level:
+		return
+	else:
+		var new_atlas : Vector2i = Vector2i(atlas_coord.x +1, atlas_coord.y)
+		handle_seed(tile_map_pos, level +1, new_atlas, final_seed_level)
